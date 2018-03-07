6615b9e345bb89cdb09ccfc1c4c9bab4836b5b0a
Ernest <ernestwang61@gmail.com>
2018-03-08 01:04:49 +0800

    [TissueBox] WEMOS: Pull status from firebase & send to processing via serial comm

--- TissueBox/TissueBox.ino
+++ TissueBox/TissueBox.ino
@@ -8,32 +8,38 @@
 #define firebaseURL "objethnographic.firebaseio.com" //
 #define authCode "gLixyhZ0JcJmuaNzs2N7VjPAT9ORMMxxhag4q94C" //資料庫密鑰
 String chipID = "tissue" ; //(資料庫第一層)控制的物件名稱
-String child = "situation" ; //子變數名稱
+String child_1 = "play" ; //子變數名稱
+String child_2 = "record" ;
 String data = "data" ; //上傳的資料
+
+bool DEBUG = false;
+
+bool play_state = false;
+bool record_state = false;
 
 //Wifi 連接 function
 void setupWifi(){
-  Serial.print("Trying to connect to ");
-  Serial.println(wifissid);
+  // Serial.print("Trying to connect to ");
+  // Serial.println(wifissid);
     // attempt to connect to Wifi network:
   WiFi.begin(wifissid, wifipass);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");  
+    // Serial.print(".");  
     }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println(WiFi.macAddress());
-  Serial.print("IP:");
-  Serial.println(WiFi.localIP());
-  Serial.print("Subnet:");
-  Serial.println(WiFi.subnetMask());
-  Serial.print("Gateway:");
-  Serial.println(WiFi.gatewayIP());
-  Serial.print("Channel:");
-  Serial.println(WiFi.channel());
-  Serial.print("Status:");
-  Serial.println(WiFi.status());
+  // Serial.println("");
+  // Serial.println("WiFi connected");
+  // Serial.println(WiFi.macAddress());
+  // Serial.print("IP:");
+  // Serial.println(WiFi.localIP());
+  // Serial.print("Subnet:");
+  // Serial.println(WiFi.subnetMask());
+  // Serial.print("Gateway:");
+  // Serial.println(WiFi.gatewayIP());
+  // Serial.print("Channel:");
+  // Serial.println(WiFi.channel());
+  // Serial.print("Status:");
+  // Serial.println(WiFi.status());
   } // wifisetup
 
 void setupFirebase(){
@@ -44,10 +50,13 @@ void setupFirebase(){
 void PullData(){
   String path = chipID ;
   FirebaseObject object = Firebase.get(path);
-  String value = object.getString(child);
-  Serial.println(child+" = ");
-  Serial.println(value);
-  delay(1000);
+  play_state = object.getBool(child_1);
+  record_state = object.getBool(child_2);
+  // Serial.print(child_1+" = ");
+  // Serial.println(play_state);
+  // Serial.print(child_2+" = ");
+  // Serial.println(record_state);
+  delay(500);
 }
 
 //firenase Push funciton
@@ -57,22 +66,83 @@ void PushData (){
   //   Firebase.push("temperature", temperatureObject);
   // handle error
   if (Firebase.failed()) {
-      Serial.print("pushing true failed:");
-      Serial.println(Firebase.error());  
+      // Serial.print("pushing true failed:");
+      // Serial.println(Firebase.error());  
       return;
    }
-  delay(3000);
+  delay(500);
 }
 
 void setup() {
   Serial.begin(115200);
   setupWifi();
   setupFirebase();
-  }
+}
 
+
+int flip_play;
+int flip_record;
+int flip_save;
 void loop() {
   PullData();
-  Serial.println("get!");
+  // Serial.println("get!");
   PushData();
-  Serial.println("push!");
+  // Serial.println("push!");
+  
+  int playTrigger = 1;
+  int recordTrigger = 3;
+  int saveTrigger = 4;
+
+  if(play_state == false)
+    flip_play = 0;
+  else if(play_state == true){
+    if(flip_play == 0){
+      Serial.write(playTrigger);
+      flip_play = 1;
+    }
   }
+  if(DEBUG){
+    Serial.print("flip_play =");
+    Serial.println(flip_play);
+  }
+  delay(10);
+
+  if(record_state == false){
+    flip_record = 0;
+    
+    if(flip_save == 0){
+      Serial.write(saveTrigger);
+      flip_save = 1;
+    }
+  }
+  else if(record_state == true){
+    if(flip_record == 0){
+      Serial.write(recordTrigger);
+      flip_record = 1;
+    }
+
+    flip_save = 0;
+  }
+  if(DEBUG){
+    Serial.print("flip_record =");
+    Serial.println(flip_record);
+  }
+  delay(10);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
